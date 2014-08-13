f675adb46b86a4f2ec6d3b148c07d0e9423d1015
安倍裕貴 <Yukiabe@abenoyuki-no-MacBook-Air-2.local>
2014-08-13 10:06:54 +0800

    Initial Commit

---
+++ JHotelList/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  JHotelList
+//
+//  Created by 安倍裕貴 on 2014/08/13.
+//  Copyright (c) 2014年 Yuki Abe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
