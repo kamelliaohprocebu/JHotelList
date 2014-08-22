6c7456a1abb36fbf8e8438f5716b35b8ccbe5d38
kamelliaohprocebu <kamelliaohprocebu@gmail.com>
2014-08-22 11:32:28 +0800

    こみっと

---
+++ hotelListTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  hotelListTableViewCell.h
+//  JHotelList
+//
+//  Created by 安倍裕貴 on 2014/08/22.
+//  Copyright (c) 2014年 Yuki Abe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface hotelListTableViewCell : UITableViewCell
+
+@end
---
+++ hotelListTableViewCell.m
@@ -0,0 +1,25 @@
+//
+//  hotelListTableViewCell.m
+//  JHotelList
+//
+//  Created by 安倍裕貴 on 2014/08/22.
+//  Copyright (c) 2014年 Yuki Abe. All rights reserved.
+//
+
+#import "hotelListTableViewCell.h"
+
+@implementation hotelListTableViewCell
+
+- (void)awakeFromNib
+{
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
